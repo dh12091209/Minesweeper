41ef7818f835284a6070acbeeafb2afd058ea2ea
freshmen <freshmen@damien-hs.edu>
2022-09-09 14:12:53 -0700

    added number tiles

--- core/src/com/dohyun/GameBoard.java
+++ core/src/com/dohyun/GameBoard.java
@@ -100,6 +100,7 @@ public class GameBoard {
                 if(board[randomRow][randomCol] == EmptyTile){
                     board[randomRow][randomCol] = BOMB+10;
                     bombCount++;
+                    System.out.println(".");
                 }
             }
         }
